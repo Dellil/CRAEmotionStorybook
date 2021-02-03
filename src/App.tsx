6c6c02dedea53484f1b774b8533b11fe8f70984b
Dellil <aksaksdm@naver.com>
2021-02-03 13:53:04 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
 import logo from './logo.svg';
 import './App.css';
 
+const Div = styled.div`
+  color:white;
+`;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Div css={css`background-color: black`}>asdfasdf</Div>;
 }
 
 export default App;
